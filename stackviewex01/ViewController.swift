dbee7475b878814ea87e75ca7e620823a59c4d79
Madiapps <madiapps@Mac-mini-de-Madiapps.local>
2022-07-26 09:14:53 +0300

    Init design

---
+++ stackviewex01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stackviewex01
+//
+//  Created by Madiapps on 25/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
